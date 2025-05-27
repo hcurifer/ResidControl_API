334e280e9a1b95aa7f1e4e0ade79f051d68205fa
hcurifer <hcurifer@myuax.com>
2025-05-27 11:35:24 +0200

    Se añade edad
    se añade edad a los modelos de residente

--- app/models/residente.py
+++ app/models/residente.py
@@ -7,6 +7,7 @@ class Residente(Base):
     id_residente = Column(Integer, primary_key=True, index=True)
     nombre = Column(String(100), nullable=False)
     apellidos = Column(String(100), nullable=False)
+    edad = Column(Integer, nullable=True)
     estado = Column(String(20), nullable=False)
     habitacion = Column(String(10), nullable=False)
     ubicacion = Column(String(100), nullable=True)
