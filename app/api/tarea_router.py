3bd16701b96687106d0f617379625a860b508f33
hcurifer <hcurifer@myuax.com>
2025-05-31 07:03:34 +0200

    actualizacion
    ahora si inyecta las taras el probleama era el estado

--- app/api/tarea_router.py
+++ app/api/tarea_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional, Literal
 from datetime import date
 from app.schemas.tarea import TareaCreate, TareaOut
 from app.db.session import SessionLocal
@@ -35,6 +35,17 @@ def crear(tarea: TareaCreate, db: Session = Depends(get_db)):
 def listar(db: Session = Depends(get_db)):
     return obtener_tareas(db)
 
+# Filtrar tareas por enfermero, fecha, turno y estado
+@router.get("/filtrar", response_model=List[TareaOut])
+def filtrar_tareas(
+    id_usuario: int,
+    fecha: date,
+    estado: Literal["pendiente", "completada"],  
+    id_turno: Optional[int] = None,              
+    db: Session = Depends(get_db)
+):
+    return obtener_tareas_por_filtro(db, id_usuario, fecha, estado, id_turno)
+
 # Obtener tarea por ID
 @router.get("/{id_tarea}", response_model=TareaOut)
 def obtener(id_tarea: int, db: Session = Depends(get_db)):
@@ -51,13 +62,3 @@ def cambiar_estado(id_tarea: int, nuevo_estado: str, db: Session = Depends(get_d
         raise HTTPException(status_code=404, detail="Tarea no encontrada")
     return tarea
 
-# Filtrar tareas por enfermero, fecha, turno y estado
-@router.get("/filtrar", response_model=List[TareaOut])
-def filtrar_tareas(
-    id_usuario: int,
-    fecha: date,
-    id_turno: int,
-    estado: str = Query(..., regex="^(pendiente|completada)$"),
-    db: Session = Depends(get_db)
-):
-    return obtener_tareas_por_filtro(db, id_usuario, fecha, id_turno, estado)
